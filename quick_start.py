2d7df88f51626020ba3dd5fe87920700ad31172c
tzondo023 <tzondo023@student.wethinkcode.co.za>
2024-01-18 15:54:36 +0200

    event creation created an error

--- quick_start.py
+++ quick_start.py
@@ -7,7 +7,42 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 # from googleapiclient.errors import HttpErro
 
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+# https://developers.google.com/calendar/api/auth
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+
+def create_event():
+    # new codee
+    event = {
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2015-05-28T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2023-01-19T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print(f"Event created: {event.get('htmlLink')}")
 
 
 if __name__ == "__main__":
@@ -29,3 +64,23 @@ if __name__ == "__main__":
             # Save the credentials for the next run
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy="startTime",
+        ).execute()
+    )
+
+    events = events_result.get("items", [])
+
+    print(events)
+
+    create_event()
+
